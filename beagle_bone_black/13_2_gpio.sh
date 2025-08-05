f9473760b27fb652505f3cb0182770603e9a0e32
parikshithks <parikshithks02@gmail.com>
2025-08-05 16:05:24 +0530

    changes

--- beagle_bone_black/13_2_gpio.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc 5_C_Code_GPIO_Output.c -o gpio_output_13_2
--- beagle_bone_black/spi_bash.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-config-pin P9_17 spi_cs
-config-pin P9_18 spi
-config-pin P9_21 spi
-config-pin P9_22 spi_sclk
-
-gcc 12_spi_test.c -o output_12
